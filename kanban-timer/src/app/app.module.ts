ec4b627c8bf34d576fa20113608c158bc9250feb
mac <mac@Henry.socal.rr.com>
2019-10-20 22:05:43 -0700

    Adding inital project source code

---
+++ kanban-timer/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Angular Material Imports
+import { MatCardModule } from '@angular/material/card';
+
+// Angular Module & Component Imports
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
